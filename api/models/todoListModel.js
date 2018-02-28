216f5ce5aa8449c891d80b3e3be766e5ba92ff2c
user <user@ELPBLR-040.evolvus.com>
2018-02-28 12:20:15 +0530

    third

--- api/models/todoListModel.js
+++ api/models/todoListModel.js
@@ -3,23 +3,23 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 
-var TaskSchema = new Schema({
-  name: {
-    type: String,
-    required: 'Kindly enter the name of the task'
-  },
-  Created_date: {
-    type: Date,
-    default: Date.now
-  },
-  status: {
-    type: [{
-      type: String,
-      enum: ['pending', 'ongoing', 'completed']
-    }],
-    default: ['pending']
-  }
-});
+// var TaskSchema = new Schema({
+//   name: {
+//     type: String,
+//     required: 'Kindly enter the name of the task'
+//   },
+//   Created_date: {
+//     type: Date,
+//     default: Date.now
+//   },
+//   status: {
+//     type: [{
+//       type: String,
+//       enum: ['pending', 'ongoing', 'completed']
+//     }],
+//     default: ['pending']
+//   }
+// });
 
 var accbalSchema = new Schema({
   account: {
